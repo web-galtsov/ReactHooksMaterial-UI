e8b8b70e0d747c42028291f444fed7463d2a5b45
Oxana <oksana.galtsov@gmail.com>
2021-01-04 18:36:40 +0200

    add select form

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import './css/App.css';
+import {
+    AppHeader,
+    AppStats
+} from './StileSide';
+import {
+    MenuItem,
+    FormControl,
+    Select
+} from '@material-ui/core';
+
+// https://disease.sh/v3/covid-19/countries
 
 function App() {
-  return (
+    const [countries, setCountries] = useState([]);
+    const [country, setCountry] = useState('worldwide');
+
+    useEffect(() => {
+        const getCountriesData = async () => {
+            await fetch('https://disease.sh/v3/covid-19/countries')
+                .then((response) => response.json())
+                .then((data) => {
+                    // noinspection JSUnresolvedVariable
+                    const countries = data.map((country) => (
+                       {
+                            name: country.country,
+                            value: country.countryInfo.iso2,
+                       }));
+                    setCountries(countries);
+                });
+              };
+                getCountriesData();
+            }, []);
+
+        const onCountryChange = (event) => {
+            const countryCode = event.target.value;
+            setCountry(countryCode);
+        };
+
+  // noinspection JSUnresolvedVariable
+    return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <AppHeader>
+            <h1>Covid - 19 TRACKER</h1>
+                <FormControl className='app_dropdown'>
+                    <Select variant='outlined' onChange={onCountryChange} value ={country}>
+                        <MenuItem value='worldwide'>Worldwide</MenuItem>
+                        { countries.map((country) => (
+                             <MenuItem  value={country.value}>{country.name}</MenuItem>
+
+                        ))}
+                    </Select>
+                </FormControl>
+        </AppHeader>
+        <AppStats>
+
+        </AppStats>
     </div>
   );
 }
-
 export default App;
---
+++ src/StileSide.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+export const AppHeader = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 20px;
+`;
+export const AppStats = styled.div`
+
+`;
